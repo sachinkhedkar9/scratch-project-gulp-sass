7b359b81886895734a0b1409b5f37c8fbde04e85
Sachin Khedkar <sachink2@outlook.com>
2016-12-07 19:04:14 +0530

    json constructed

--- src/js/controllers/ctoolController.js
+++ src/js/controllers/ctoolController.js
@@ -2,29 +2,47 @@ skeleton.controller('ctoolController', ["$scope", "$location", function($scope, 
   $scope.getTables = "hideTable";    
   $scope.showNextFlag = false;
 
-  $scope.users = [
+  $scope.commercial = {};
+  $scope.commercial.users = [
     {
-      name:'Sachin',
+      name:'Adam Gilli',
        id:1
     },
     {
-      name:'Jay', 
+      name:'Ricky Punter', 
       id:2
     },
     {
-      name:'grid-page', 
+      name:'Optimus Prime', 
       id:3
     },
     {
-      name:'Sathish', 
+      name:'Peter Parker', 
       id:4
     }
   ];
+  
+  $scope.commercial.company = ['10 GRAD(37669)', '100 BEST(39551)', '10DUKE (38660)', '118811 (39258)'];
+  $scope.commercial.billingLocation = ["Mobile 365 Inc.", "Mobile 365 South Africa.", "Mobileway Australia", "Mobileway China", "Mobileway India" ];
+  $scope.commercial.serviceLevel = ['Standard', 'Premium'];
+  $scope.commercial.trafficType = ['General','Campaign','Low Latency','Time Sensitive'];
+  $scope.commercial.interfaceType = ['HTTP', 'SMPP', 'SMTP'];
 
-  $scope.opt = $scope.users[0];
+  // Once the rest calls are set then we do not need this json 
+  $scope.json = {
+    requester       : "Venkat",
+    acctMgr         : $scope.commercial.users[0],
+    company         : $scope.commercial.company[0],
+    billingLocation : $scope.commercial.billingLocation[0],
+    serviceLevel    : $scope.commercial.serviceLevel[0],
+    trafficType     : $scope.commercial.trafficType[0],
+    interfaceType   : $scope.commercial.interfaceType[0],
+    techName        : "Test Account"
+  };
+
+  // $scope.opt = $scope.users[0];
 
   $scope.showNextTables = function(flag){
-    console.log('flag = ', flag);
     $scope.showNextFlag = !$scope.showNextFlag;
     if(flag){
       $scope.showTables();
@@ -35,21 +53,10 @@ skeleton.controller('ctoolController', ["$scope", "$location", function($scope, 
 
   $scope.showTables = function(){
     $scope.getTables = "showTable";
-    $scope.replaceEditablesWithStrings();
   };
 
   $scope.hideTables = function(){ 
     $scope.getTables = "hideTable";
-    $scope.replaceStringsWithEditables();        
-  };
-
-  $scope.replaceEditablesWithStrings = function(){
-    console.log('in replaceEditablesWithStrings');
-    
-  };
-
-  $scope.replaceStringsWithEditables = function(){
-    console.log('in replaceStringsWithEditables');
   };
 
   $scope.logout = function(){
