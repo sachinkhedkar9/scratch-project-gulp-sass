8b1999c1bbdec8e1325b2becc31d4fc3b936e1cb
Sachin Khedkar <sachink2@outlook.com>
2016-12-08 18:55:50 +0530

    minor changes

--- src/js/controllers/loginController.js
+++ src/js/controllers/loginController.js
@@ -1,4 +1,4 @@
-skeleton.controller('loginController', ["$scope", "$location", function($scope, $location){
+skeleton.controller('loginController', ["$scope", "$location", "$http", function($scope, $location){
     $scope.userLogin = {
         username: "sachin",
         password: "sachin123"
@@ -9,7 +9,33 @@ skeleton.controller('loginController', ["$scope", "$location", function($scope, 
         console.log('user obj - ', $scope.userLogin);
         if($scope.userLogin.username === $scope.loginUser.username && $scope.userLogin.password === $scope.loginUser.password){
             console.log("logging in", $location.path());
-            $location.path('/ctool/');
+            // angular.element('#errorAlert').trigger('click');
+            var alert = $('#successAlert');
+            alert.modal('show');
         }
     };
+
+    $scope.navigateToApp = function(){
+        $location.path('/ctool/');        
+    };
+
+    // $scope.login = function(){
+    //     var login = {
+    //         method: 'POST',
+    //         url: '',
+    //         headers: {
+    //             'Content-Type': "application/json"
+    //         },
+    //         data: $scope.loginUser
+    //     }
+
+    //     $http(login).then(function(successData){
+    //         // post data success
+    //         console.log('success data : ',successData);
+    //         $location.path('/ctool/');              // navigate to ctool home page.
+    //     }).then(function(errorData){
+    //         console.log('error data : ', errorData);
+    //         angular.element('#errorAlert').trigger('click');
+    //     });
+    // };
 }]);
