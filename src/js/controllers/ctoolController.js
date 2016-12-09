1e63931b82110d615d6d6ffd2ef232390b398fc3
Sachin Khedkar <sachink2@outlook.com>
2016-12-09 17:04:53 +0530

    changes suggested

--- src/js/controllers/ctoolController.js
+++ src/js/controllers/ctoolController.js
@@ -29,28 +29,7 @@ skeleton.controller('ctoolController', ["$scope", "$location", function($scope, 
   $scope.commercial.serviceLevel = ['Standard', 'Premium'];
   $scope.commercial.trafficType = ['General','Campaign','Low Latency','Time Sensitive'];
   $scope.account.interfaceType = ['HTTP', 'SMPP', 'SMTP'];
-  $scope.country = [
-    {
-      code: 1,
-      name: 'India'
-    },
-    {
-      code: 2,
-      name: 'United States Of America'
-    },
-    {
-      code: 3,
-      name: 'Russia'
-    },
-    {
-      code: 4,
-      name: 'Germany'
-    },
-    {
-      code: 5,
-      name: 'China'
-    }
-  ];
+  $scope.country = ['India', 'United States Of America', 'Russia', 'Germany', 'China'];
 
   $scope.technical.contacts = [
     {
@@ -58,66 +37,67 @@ skeleton.controller('ctoolController', ["$scope", "$location", function($scope, 
       contactName: 'xor',
       name: 'Xoriant', 
       email: 'email@xoriant.com',
-      country: {
-                  code: 1,
-                  name: 'India'
-               },
-      mobile: '12324564789',
-      directMobile: '123456789'
+      country: $scope.country[0],               
+      mobilePhoneNumber: '12324564789',
+      directPhoneNumber: '123456789'
     },
     {
       id: 101,
       contactName: 'CTS',
       name: 'Cognizant', 
       email: 'email@Cognizant.com',
-      country: '',
-      mobile: '123456789',
-      directMobile: '12345645'
+      country: $scope.country[4],
+      mobilePhoneNumber: '123456789',
+      directPhoneNumber: '12345645'
     },
     {
       id: 102,
       contactName: 'TCS',
       name: 'Tata Consultancy Services', 
       email: 'email@tcs.com',
-      country: '',
-      mobile: '1234567890',
-      directMobile: '678967'
+      country: $scope.country[2],
+      mobilePhoneNumber: '1234567890',
+      directPhoneNumber: '678967'
     },
     {
       id: 103,
       contactName: 'acc',
       name: 'Accenture', 
       email: 'email@Accenture.com',
-      country: '',
-      mobile: '9876543211',
-      directMobile: '8967890'
+      country: $scope.country[3],
+      mobilePhoneNumber: '9876543211',
+      directPhoneNumber: '8967890'
     },
     {
       id: 104,
       contactName: 'cyb',
       name: 'Cybage', 
       email: 'email@Cybage.com',
-      country: '',
-      mobile: '98765437654',
-      directMobile: '54411653'
+      country: $scope.country[5],
+      mobilePhoneNumber: '98765437654',
+      directPhoneNumber: '54411653'
     }
   ];
 
-  $scope.existingAccounts = ['HDFC','HSBC','Bank Of America','Bank Of China'];
+  var newContactData = $scope.getContactData();
 
+  $scope.existingAccounts = ['118118_Bulk_HTTP','118119_Bulk_HTTP','1181520_Bulk_HTTP','118121_Bulk_HTTP'];
+
+  $scope.requesterName = 'Venkat';
 
   // Once the rest calls are set then we do not need this json 
   $scope.json = {
-    requester       : "Venkat",
+    requester       : $scope.requesterName,
     acctMgr         : $scope.commercial.users[0],
     company         : $scope.commercial.company[0],
     billingLocation : $scope.commercial.billingLocation[0],
     serviceLevel    : $scope.commercial.serviceLevel[0],
     trafficType     : $scope.commercial.trafficType[0],
     interfaceType   : $scope.account.interfaceType[0],
-    technicalName   : "",
+    techName        : "",
     existingCompany : "Xoriant sol.",
-    contact         : $scope.technical.contacts[0]
+    contact         : $scope.technical.contacts[0],
+    existingAccount : $scope.existingAccounts[0]
   };
 
   // $scope.opt = $scope.users[0];
@@ -148,8 +128,46 @@ skeleton.controller('ctoolController', ["$scope", "$location", function($scope, 
     console.log('ng-model - ', $scope.json.contact);
   };
 
+  $scope.getContactData = function(){
+    return $http.get('/techContact/123')
+    .then($scope.succesGetContactData)  
+    .then($scope.errorContactData)
+  };
+
+  $scope.succesGetContactData = function(data){
+      console.log('data = ', data);
+      return data;
+  };
+
+  $scope.errorContactData = function(data){
+      console.log('data = ', data);
+  };
+
   $scope.create = function(){
     console.log('json obtained - ', $scope.json);
+
+    var necessaryData = {};
+    // necessaryData.
+
+    // http://localhost:8080/accounts
+    var login = {
+            method: 'POST',
+            url: 'http://localhost:8080/accounts',
+            headers: {
+                'Authorization': 'Basic ' + btoa('admin'+':'+'password'),
+                'Content-type': 'application/json'
+            },
+            data: $scope.json
+        };
+
+        $http(login).then(function(successData){
+            // post data success
+            console.log('success data : ',successData);
+            $location.path('/ctool/');              // navigate to ctool home page.
+        }).then(function(errorData){
+            console.log('error data : ', errorData);
+            angular.element('#errorAlert').trigger('click');
+        });
   };
 
 }]);
--- src/js/controllers/loginController.js
+++ src/js/controllers/loginController.js
@@ -1,42 +1,42 @@
-skeleton.controller('loginController', ["$scope", "$location", "$http", function($scope, $location){
-    $scope.userLogin = {
-        username: "sachin",
-        password: "sachin123"
-    };
-
-    $scope.login = function(){
-        console.log('login obj - ', $scope.loginUser);
-        console.log('user obj - ', $scope.userLogin);
-        if($scope.userLogin.username === $scope.loginUser.username && $scope.userLogin.password === $scope.loginUser.password){
-            console.log("logging in", $location.path());
-            // angular.element('#errorAlert').trigger('click');
-            // var alert = $('#successAlert');
-            // alert.modal('show');
-            $scope.navigateToApp();
-        }
-    };
-
-    $scope.navigateToApp = function(){
-        $location.path('/ctool/');        
-    };
+skeleton.controller('loginController', ["$scope", "$location", "$http", function($scope, $location, $http){
+    // $scope.userLogin = {
+    //     username: "sachin",
+    //     password: "sachin123"
+    // };
 
     // $scope.login = function(){
-    //     var login = {
-    //         method: 'POST',
-    //         url: '',
-    //         headers: {
-    //             'Content-Type': "application/json"
-    //         },
-    //         data: $scope.loginUser
+    //     console.log('login obj - ', $scope.loginUser);
+    //     console.log('user obj - ', $scope.userLogin);
+    //     if($scope.userLogin.username === $scope.loginUser.username && $scope.userLogin.password === $scope.loginUser.password){
+    //         console.log("logging in", $location.path());
+    //         // angular.element('#errorAlert').trigger('click');
+    //         // var alert = $('#successAlert');
+    //         // alert.modal('show');
+    //         $scope.navigateToApp();
     //     }
-
-    //     $http(login).then(function(successData){
-    //         // post data success
-    //         console.log('success data : ',successData);
-    //         $location.path('/ctool/');              // navigate to ctool home page.
-    //     }).then(function(errorData){
-    //         console.log('error data : ', errorData);
-    //         angular.element('#errorAlert').trigger('click');
-    //     });
     // };
+
+    // $scope.navigateToApp = function(){
+    //     $location.path('/ctool/');        
+    // };
+
+    $scope.login = function(){
+        var login = {
+            method: 'POST',
+            url: '/login',
+            headers: {
+                'Content-Type': "application/x-www-form-urlencoded"
+            },
+            data: JSON.stringify($scope.loginUser)
+        };
+
+        $http(login).then(function(successData){
+            // post data success
+            console.log('success data : ',successData);
+            $location.path('/ctool/');              // navigate to ctool home page.
+        }).then(function(errorData){
+            console.log('error data : ', errorData);
+            angular.element('#errorAlert').trigger('click');
+        });
+    };
 }]);
